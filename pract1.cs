02151c7e8a1cd9aa25bf6f09fbb9b7949b489556
vaishnavipatil11 <107455498+vaishnavipatil11@users.noreply.github.com>
2022-11-30 11:59:43 +0530

    Add files via upload

---
+++ pract1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_PreInit(object sender, EventArgs e)
+    {
+        //Work and It will assign the values to label.
+        Label1.Text = Label1.Text + "<br/>" + "PreInit";
+    }
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        //Work and It will assign the values to label.
+        Label1.Text = Label1.Text + "<br/>" + "Init";
+    }
+    protected void Page_InitComplete(object sender, EventArgs e)
+    {
+        //Work and It will assign the values to label.
+        Label1.Text = Label1.Text + "<br/>" + "InitComplete";
+    }
+    protected override void OnPreLoad(EventArgs e)
+ {
+ //Work and It will assign the values to label.
+
+ Label1.Text = Label1.Text + "<br/>" + "PreLoad";
+ }
+    protected void Page_Load(object sender, EventArgs e)
+ {
+
+ //Work and It will assign the values to label.
+ Label1.Text = Label1.Text + "<br/>" + "Load";
+ }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        //Work and It will assign the values to label.
+        Label1.Text = Label1.Text + "<br/>" + "btnSubmit_Click";
+    }
+    protected void Page_LoadComplete(object sender, EventArgs e)
+    {
+        //Work and It will assign the values to label.
+        Label1.Text = Label1.Text + "<br/>" + "LoadComplete";
+    }
+    protected override void OnPreRender(EventArgs e)
+    {
+        
+        Label1.Text = Label1.Text + "<br/>" + "PreRender";
+    }
+    protected override void OnSaveStateComplete(EventArgs e)
+ {
+ 
+ Label1.Text = Label1.Text + "<br/>" + "SaveStateComplete";
+ }
+    protected void Page_UnLoad(object sender, EventArgs e)
+ {
+ 
+ Label1.Text = Label1.Text + "<br/>" + "UnLoad";
+ }
+
+}
---
+++ pract12.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+public partial class _Default : System.Web.UI.Page
+{
+ protected void Page_Load(object sender, EventArgs e)
+ {
+ DataTable tempTable = new DataTable();
+ 
+ tempTable.Columns.Add("ID");
+ tempTable.Columns.Add("NAME");
+ 
+ DataRow newrow = tempTable.NewRow();
+ newrow[0] = 0;
+ newrow[1] = "Avinash";
+ tempTable.Rows.Add(newrow);
+ newrow = tempTable.NewRow();
+ newrow[0] = 1;
+ newrow[1] = "Kavita";
+ tempTable.Rows.Add(newrow);
+ 
+ Response.Write("Original Table"+"<br>");
+
+ for (int i = 0; i < tempTable.Rows.Count; i++)
+ {
+ Response.Write(tempTable.Rows[i][0].ToString() + "--->" +
+tempTable.Rows[i][1].ToString()+"<br>");
+ }
+ // if you want edit data from DataTable
+ Response.Write("Modified Table" + "<br>");
+ for (int i = 0; i < tempTable.Rows.Count; i++)
+ {
+ if (tempTable.Rows[i][1].ToString() == "Kavita")
+ {
+ tempTable.Rows[i][1] = "Ganesh";
+ }
+ Response.Write(tempTable.Rows[i][0].ToString() + "--->" +
+tempTable.Rows[i][1].ToString() + "<br>");
+ } 
+     Response.Write("After Deleting row from Table" + "<br>");
+ for (int i = 0; i < tempTable.Rows.Count; i++)
+
+ {
+ if (tempTable.Rows[i][1].ToString() == "Ganesh")
+ {
+ tempTable.Rows[i].Delete();
+ }
+ }
+ for (int i = 0; i < tempTable.Rows.Count; i++)
+ {
+ Response.Write(tempTable.Rows[i][0].ToString() + "--->" +
+tempTable.Rows[i][1].ToString() + "<br>");
+ }
+ }
+}
